3c3869b0569f7ddb638b7faf05e0bdd214452c44
Your Name <you@example.com>
2025-05-26 11:46:16 +0200

    Update: Modified setup.py for version bump and deployment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="circuiteasy",
-    version="0.1.0",
+    version="0.2.0",
     description="ELE142 and general circuit analysis tools for fast, clear Python calculations.",
     author="Hareth Al-jomaa",
     author_email="harethaljomaa@outlook.com",
